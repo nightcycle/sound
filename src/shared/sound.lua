8b473aaca297497ad3f5240faf2a4295a86be456
Nightcycle Studios <coyer@nightcycle.us>
2021-12-13 05:40:41 -0500

    wally publish

---
+++ src/shared/sound.lua
@@ -0,0 +1,60 @@
+local packages = game.ReplicatedStorage:WaitForChild('Packages')
+
+local maidConstructor = require(packages:WaitForChild('maid'))
+local library = require(packages:WaitForChild('Library'))
+
+local sounds = library.new("Sounds")
+for i, soundGroup in ipairs(game:WaitForChild("SoundService"):GetChildren()) do
+	for _, soundInst in ipairs(soundGroup:GetChildren()) do
+		if soundInst:IsA("Sound") then
+			soundInst.SoundGroup = soundGroup
+			sounds:Set(soundGroup.Name.."/"..soundInst.Name, soundInst)
+		end
+	end
+end
+
+local Object = {}
+Object.__index = Object
+
+function Object:Destroy()
+	self._maid:Destroy()
+end
+
+function Object:Play()
+	self.Instance:Play()
+end
+
+function Object:Fire()
+	self.Instance.PlayOnRemove = true
+	self:Destroy()
+end
+
+function Object:Resume()
+	self.Instance:Resume()
+end
+
+function Object:Pause()
+	self.Instance:Pause()
+end
+
+function Object:Stop()
+	self.Instance:Stop()
+end
+
+local constructor = {}
+
+function constructor.new(soundKey, parentInst)
+	local self = setmetatable({}, Object)
+
+	self.Instance = sounds:Get(soundKey):Clone()
+	self._maid:GiveTask(self.Instance)
+	if parentInst then
+		self.Instance.Parent = parentInst
+	end
+	self._maid = maidConstructor.new()
+	self._maid:GiveTask(self)
+
+	return self
+end
+
+return constructor
